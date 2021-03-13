9ed71802e4ed1fa7ef846feb050341f0e8e6717a
Dmitry V. Sokolov <ha@haqr.eu>
2021-03-13 17:37:06 +0100

    cmake files; dummy Catch2 test

---
+++ stlbfgs.h
@@ -0,0 +1,9 @@
+#ifndef __STLBFGS_H__
+#define __STLBFGS_H__
+
+#include <vector>
+namespace STLBFGS {
+}
+
+#endif //__STLBFGS_H__
+
---
+++ tests/test-stlbfgs.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <iostream>
+#include "stlbfgs.h"
+
+using namespace STLBFGS;
+static const double ftol = 1e-13;
+
+TEST_CASE("foo", "[bar]") {
+        REQUIRE(true);
+}
+
