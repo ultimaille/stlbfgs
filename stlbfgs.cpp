e746c94b2932cd81ed9b66e64c5c7e84c7c3c289
Dmitry V. Sokolov <ha@haqr.eu>
2025-11-27 14:45:11 +0100

    warning

--- stlbfgs.cpp
+++ stlbfgs.cpp
@@ -124,8 +124,8 @@ namespace STLBFGS {
 #if defined(_OPENMP) && _OPENMP>=200805
 #pragma omp parallel for reduction(max:gmax_)
 #endif
-            for (int i=0; i<n; i++)
-                gmax_ = std::max(gmax_, std::abs(g[i]));
+            for (int j=0; j<n; j++)
+                gmax_ = std::max(gmax_, std::abs(g[j]));
             if (gmax_ <= gmax) {
                 if (verbose) std::cerr << "max{|g_i|, i = 1, ..., n} <= " <<  gmax << std::endl;
                 return true;
